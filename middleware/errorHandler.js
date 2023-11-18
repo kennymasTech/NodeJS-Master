92ba37e42090fb847d5c913f24b2279d8775f229
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-11-18 23:42:28 +0100

    a

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -5,6 +5,6 @@ const errorHandler = (err, req, res, next) => {
     logEvents(`${err.name}: ${err.message}`, 'errLog.txt');
     console.error(err.stack)
     res.status(500).send(err.message);
-}
+};
 
 module.exports = errorHandler;
